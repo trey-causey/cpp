f5b0a5975014ad29db490a158e788a82dd145de0
trey-causey <trey.causey@outlook.com>
2020-09-26 07:59:40 -0400

    nucleotide-count tested and working

---
+++ nucleotide-count/cmake-build-debug/CMakeFiles/nucleotide-count.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/TJC/Exercism/cpp/nucleotide-count/nucleotide_count.cpp" "C:/Users/TJC/Exercism/cpp/nucleotide-count/cmake-build-debug/CMakeFiles/nucleotide-count.dir/nucleotide_count.cpp.obj"
+  "C:/Users/TJC/Exercism/cpp/nucleotide-count/nucleotide_count_test.cpp" "C:/Users/TJC/Exercism/cpp/nucleotide-count/cmake-build-debug/CMakeFiles/nucleotide-count.dir/nucleotide_count_test.cpp.obj"
+  "C:/Users/TJC/Exercism/cpp/nucleotide-count/test/tests-main.cpp" "C:/Users/TJC/Exercism/cpp/nucleotide-count/cmake-build-debug/CMakeFiles/nucleotide-count.dir/test/tests-main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ nucleotide-count/cmake-build-debug/CMakeFiles/nucleotide-count.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nucleotide-count.dir/nucleotide_count.cpp.obj"
+  "CMakeFiles/nucleotide-count.dir/nucleotide_count_test.cpp.obj"
+  "CMakeFiles/nucleotide-count.dir/test/tests-main.cpp.obj"
+  "nucleotide-count.exe"
+  "nucleotide-count.exe.manifest"
+  "nucleotide-count.lib"
+  "nucleotide-count.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nucleotide-count.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
